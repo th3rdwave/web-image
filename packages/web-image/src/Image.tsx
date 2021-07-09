b3f2d6804412d58b61d8e72d56db369018291bd5
Janic Duplessis <janicduplessis@gmail.com>
2021-07-09 18:01:49 +0000

    Support tintColor and blurRadius

--- packages/web-image/src/Image.tsx
+++ packages/web-image/src/Image.tsx
@@ -3,8 +3,12 @@ import * as React from 'react';
 import {
   ImageProps as BaseImageProps,
   ImageResizeMode,
+  ImageStyle,
   ImageURISource as BaseImageURISource,
+  StyleProp,
+  StyleSheet,
   View,
+  ViewStyle,
 } from 'react-native';
 
 export interface ResponsiveImageSource {
@@ -84,6 +88,70 @@ const resizeModeToObjectFit = (
   }
 };
 
+let _filterId = 0;
+
+// https://github.com/necolas/react-native-web/blob/master/packages/react-native-web/src/exports/Image/index.js
+const createTintColorSVG = (
+  tintColor: string | null | undefined,
+  id: number | null | undefined,
+) => {
+  return tintColor != null && id != null ? (
+    <svg
+      style={{
+        position: 'absolute',
+        height: 0,
+        visibility: 'hidden',
+        width: 0,
+      }}
+    >
+      <defs>
+        {/* @ts-expect-error */}
+        <filter id={`tint-${id}`} suppressHydrationWarning={true}>
+          <feFlood floodColor={`${tintColor}`} key={tintColor} />
+          <feComposite in2="SourceAlpha" operator="atop" />
+        </filter>
+      </defs>
+    </svg>
+  ) : null;
+};
+
+const getFlatStyle = (
+  style: StyleProp<ImageStyle> | null | undefined,
+  blurRadius: number | null | undefined,
+  filterId: number | null | undefined,
+) => {
+  const flatStyle = { ...StyleSheet.flatten(style) };
+  const { filter, tintColor } = flatStyle as any;
+
+  // Add CSS filters
+  // React Native exposes these features as props and proprietary styles
+  const filters = [];
+  let _filter = null;
+
+  if (filter) {
+    filters.push(filter);
+  }
+  if (blurRadius) {
+    filters.push(`blur(${blurRadius}px)`);
+  }
+  if (tintColor && filterId != null) {
+    filters.push(`url(#tint-${filterId})`);
+  }
+
+  if (filters.length > 0) {
+    _filter = filters.join(' ');
+  }
+
+  // These styles are converted to CSS filters applied to the
+  // element displaying the background image.
+  delete flatStyle.tintColor;
+  // These styles are not supported on View
+  delete flatStyle.overlayColor;
+  delete flatStyle.resizeMode;
+
+  return [flatStyle as ViewStyle, _filter, tintColor];
+};
+
 export interface ImageProps extends Omit<BaseImageProps, 'source'> {
   /**
    * If the image should not be lazy loaded.
@@ -104,18 +172,27 @@ export const Image = React.forwardRef<View, ImageProps>(
   (
     {
       source,
-      resizeMode,
+      resizeMode: propsResizeMode,
       accessibilityLabel,
       width,
       height,
       critical,
       style,
       draggable,
+      blurRadius,
       ...others
     },
     ref,
   ) => {
+    const filterRef = React.useRef(_filterId++);
     const resolvedSource = resolveSource(source);
+    const [flatStyle, _resizeMode, filter, tintColor] = getFlatStyle(
+      style,
+      blurRadius,
+      filterRef.current,
+    );
+    const resizeMode = propsResizeMode ?? _resizeMode;
+
     return (
       <View
         ref={ref}
@@ -128,7 +205,7 @@ export const Image = React.forwardRef<View, ImageProps>(
                 : width,
             height,
           },
-          style,
+          flatStyle,
         ]}
         {...others}
       >
@@ -164,6 +241,7 @@ export const Image = React.forwardRef<View, ImageProps>(
               style={{
                 width: '100%',
                 height: '100%',
+                filter,
                 objectFit: resizeModeToObjectFit(
                   resizeMode ??
                     // When using intrinsic size use contain to avoid
@@ -182,6 +260,7 @@ export const Image = React.forwardRef<View, ImageProps>(
             />
           </picture>
         )}
+        {createTintColorSVG(tintColor, filterRef.current)}
       </View>
     );
   },
